b2d25164ce66302f3c3af2868e23e1db7d4bae1f
Kavi-axop <kavipriyan07s@gmail.com>
2026-09-19 16:18:58 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -119,6 +119,7 @@ def init_db():
         CREATE TABLE IF NOT EXISTS concepts (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT UNIQUE NOT NULL,
+            created_at TEXT NOT NULL,
             difficulty TEXT,
             what_to_do TEXT
         )
